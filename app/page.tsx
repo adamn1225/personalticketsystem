5137b0e840b8cce001b5211a2177dacba8fbde0b
adamn1225 <anoah1225@gmail.com>
2025-03-27 11:36:48 -0400

    email, name, num, added

--- app/page.tsx
+++ app/page.tsx
@@ -25,7 +25,7 @@ export default function Home() {
       <div className="absolute top-4 right-1/2 transform translate-x-1/2">
         <ModeToggle /> {/* Add the toggle */}
       </div>
-      <main className="flex flex-col justify-center items-center gap-6 md:gap-12 w-full mt-20 md:mt-10 ">
+      <main className={boomerMode ? "flex flex-col justify-center items-center gap-6 md:gap-12 w-full mt-10 md:mt-20 " : "flex flex-col justify-center items-center gap-6 md:gap-12 w-full mt-10 md:mt-0"}>
         <h1 className={`text-xl md:text-4xl ${textColor} font-bold text-center`}>
           Next Support Ticket System
         </h1>
